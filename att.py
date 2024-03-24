f9f62ba25671677ce09b1312f75e48c2d9cb2170
j-morales-code <jmorales@example.com>
2024-03-24 20:27:32 +0800

    daily attendance monitoring complete

--- att.py
+++ att.py
@@ -1,109 +1,115 @@
 from prettytable import PrettyTable
-table = PrettyTable()
-
-table.align = "l"
-
-student_record = []
-temp_list = []
-teacher_name = ""
-
-attendance_percentage = ""
-late_percentage = ""
-
-def addStudent(name):
-    newStudent = {
-        "name" : name,
-        "isPresent" : "present",
-        "isLate": ""
-    }
-    student_record.append(newStudent)
 
 
-teacher_name = input("Enter your name: ")
-print(f"Hi {teacher_name}!")
-student_data = input("Enter your list of students separated by comma: ")
-student_list = student_data.split(", ")
+
+daily_records = []
+record_day = True
+day_number = 1
 
 
-for student in student_list:
-    addStudent(student)
-
-
-#IF THERE IS/are AN ABSENT/ABSENCES
-ifAnyoneAbsent = input("Any absent y/n: ")
-
-
-while (ifAnyoneAbsent != 'y' and ifAnyoneAbsent != 'n' and ifAnyoneAbsent !='q'):
-
-    print("ERROR: Invalid input, please enter 'y' or 'n' if you need to quit the program press 'q'")
-    ifAnyoneAbsent = input("Any absent? y/n: ")
-
-
-    if(ifAnyoneAbsent == 'q'):
-        break
-
-
-if (ifAnyoneAbsent == 'y'):
-    user_input = ""
-    user_input = input("Enter names of absent students separated by commas: ")
-    temp_list = user_input.split(", ")
-
-    for student in student_record:
-        if student["name"] in temp_list:
-            student["isPresent"] = "absent"
-           
-
-elif (ifAnyoneAbsent == "n"):
-
-    attendance_percentage = "100%"
-
-
-#if there is/are late/lates
-ifAnyoneLate = input("Any late y/n: ")
-
-
-while (ifAnyoneLate != 'y' and ifAnyoneLate != 'n' and ifAnyoneLate !='q'):
-
-    print("ERROR: Invalid input, please enter 'y' or 'n' if you need to quit the program press 'q'")
-    ifAnyoneLate = input("Any late(s)? y/n: ")
-
-
-    if(ifAnyoneLate == 'q'):
-        break
-
-
-if (ifAnyoneLate == 'y'):
-    user_input = ""
+while record_day:
+    table = PrettyTable()
+    student_record = []
     temp_list = []
-    user_input = input("Enter name(s) of students to be marked as late: ")
-    temp_list = user_input.split(", ")
+    teacher_name = ""
 
-    for student in student_record:
-        if student["name"] in temp_list:
-            student["isLate"] = "late"
-           
+    attendance_percentage = ""
+    late_percentage = ""
 
-elif (ifAnyoneLate == "n"):
-
-    late_percentage = "0%"
-
-       
-
-#Preparing for report generation
-names_students = []
-attendance_status = []
-late_status = []
-
-for student in student_record:
-    names_students.append(student['name'])
-    attendance_status.append(student['isPresent'])
-    late_status.append(student['isLate'])
+    def addStudent(name):
+        newStudent = {
+            "name" : name,
+            "isPresent" : "present",
+            "isLate": ""
+        }
+        student_record.append(newStudent)
 
 
-# print(record_result) - Print Report
-table.add_column("Name of Student", names_students)
-table.add_column("Attendance Status", attendance_status)
-table.add_column("Punctuality", late_status)
-print(table)
+    teacher_name = input("Enter your name: ")
+    print(f"Hi {teacher_name}!")
+    student_data = input("Enter your list of students separated by comma: ")
+    student_list = student_data.split(", ")
 
+
+    for student in student_list:
+        addStudent(student)
+
+
+    #IF THERE IS/are AN ABSENT/ABSENCES
+    ifAnyoneAbsent = input("Any absent y/n: ")
+
+
+    while (ifAnyoneAbsent != 'y' and ifAnyoneAbsent != 'n' and ifAnyoneAbsent !='q'):
+
+        print("ERROR: Invalid input, please enter 'y' or 'n' if you need to quit the program press 'q'")
+        ifAnyoneAbsent = input("Any absent? y/n: ")
+
+
+        if(ifAnyoneAbsent == 'q'):
+            break
+
+
+    if (ifAnyoneAbsent == 'y'):
+        user_input = ""
+        user_input = input("Enter names of absent students separated by commas: ")
+        temp_list = user_input.split(", ")
+
+        for student in student_record:
+            if student["name"] in temp_list:
+                student["isPresent"] = "absent"
+            
+
+    elif (ifAnyoneAbsent == "n"):
+
+        attendance_percentage = "100%"
+
+
+    #if there is/are late/lates
+    ifAnyoneLate = input("Any late y/n: ")
+
+
+    while (ifAnyoneLate != 'y' and ifAnyoneLate != 'n' and ifAnyoneLate !='q'):
+
+        print("ERROR: Invalid input, please enter 'y' or 'n' if you need to quit the program press 'q'")
+        ifAnyoneLate = input("Any late(s)? y/n: ")
+
+
+        if(ifAnyoneLate == 'q'):
+            break
+
+
+    if (ifAnyoneLate == 'y'):
+        user_input = ""
+        temp_list = []
+        user_input = input("Enter name(s) of students to be marked as late: ")
+        temp_list = user_input.split(", ")
+
+        for student in student_record:
+            if student["name"] in temp_list:
+                student["isLate"] = "late"
+            
+
+    elif (ifAnyoneLate == "n"):
+
+        late_percentage = "0%"
+
+    
+    
+    daily_records.append(student_record)
+
+    next_day = input("Record for the next day? y/n: ")
+    if next_day == 'n' or next_day == 'q':
+        record_day = False  # Exit the loop to stop recording for more days
+    
+
+for day_records in daily_records:
+    table = PrettyTable(["Name of Student", "Attendance Status", "Punctuality"])
+    
+    for record in day_records:
+        table.add_row([record['name'], record['isPresent'], record['isLate']])
+    
+    print(f"Day {day_number}")
+    print(table)
+    print("\n")
+    day_number += 1
 
---
+++ test.py
@@ -0,0 +1,7 @@
+daily_records = [
+    {
+        "Day1": {{'name': 'john', 'isPresent': 'present', 'isLate': ''}}
+    }
+]
+
+print(daily_records[0]["Day1"])
